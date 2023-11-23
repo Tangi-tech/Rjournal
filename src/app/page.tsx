462a9b4cde06a0d6085c60cee49193b59281f3e1
Ignat Vant <ignatkld@gmail.com>
2023-11-23 19:29:08 -0300

    Header component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@/components/Header'
 import styles from './page.module.css'
-import { Button, Paper } from '@mui/material'
+import { Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Button variant="contained">хуй</Button>
+    <>
       <Header/>
-    </main>
+      <main className={styles.main}>
+        <Button variant="contained">хуй</Button>
+      </main>
+    </>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 import styles from './Header.module.scss'
+import { Paper } from '@mui/material'
 
 export const Header: React.FC = () => {
   return (
-    <div>header</div>
+    <Paper elevation={0}>header</Paper>
   )
 }
